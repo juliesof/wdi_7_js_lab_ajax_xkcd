6fb5e867227a8a361f586526196dec581f075ed4
Julie <juliesof@gmail.com>
2014-07-21 12:29:19 -0400

    Displays the title and image of 3 xkcds from 2006

--- xkcd.js
+++ xkcd.js
@@ -1,1 +1,20 @@
-// Your code here!
+$(document).ready(function() {
+
+  $.ajax({
+    url: 'http://xkcd-unofficial-api.herokuapp.com/xkcd',
+    data: {year: 2006},
+  })
+  .done(function(data) {
+    data.forEach(function(comic){
+      var $newSection = $('<section>');
+      var url = comic.img;
+      $newSection.append($('<h2>').text(comic.title));
+      $newSection.append($('<img src=' + url + '>'));
+      $('.comics').append($newSection);
+    });
+  });
+});
+
+
+
+
